b178ba4ac4e13f0c6c4701ea7c5af47bd79eae7e
Gel Lopez <nigelfranzmlopez@gmail.com>
2026-03-03 12:26:43 +0800

    Initial commit

---
+++ amaysim-app/src/main/java/com/amaysim/app/amaysim_app/ui/models/HomePage.java
@@ -0,0 +1,7 @@
+package com.amaysim.app.amaysim_app.ui.models;
+
+import com.amaysim.app.amaysim_app.ui.models.base.BasePage;
+
+public class HomePage extends BasePage {
+
+}
---
+++ amaysim-app/src/main/java/com/amaysim/app/amaysim_app/ui/models/base/BasePage.java
@@ -0,0 +1,8 @@
+package com.amaysim.app.amaysim_app.ui.models.base;
+
+public class BasePage {
+	
+	public void NavigateToUrl(String url) {
+		
+	}
+}
